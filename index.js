7b704c2abb3488cc68b0f29e16822452fe77d4c2
Pavaman Dabeer <pavamandabeer@gmail.com>
2022-08-30 21:38:32 +0530

    added routes for different social medias

--- index.js
+++ index.js
@@ -6,7 +6,40 @@ const PORT = process.env.PORT || 4000
 
 
 app.get('/', (req, res)=>{
-    res.send("Hello World!")
+    res.status(200).send("Hello World!")
+})
+
+app.get("/api/v1/instagram", (req, res)=>{
+    const instaSocial = {
+        username : 'pavamandabeer',
+        followers : '100',
+        follows : '400',
+        date : Date.now()
+    };
+
+    res.status(200).json(instaSocial)
+})
+
+app.get("/api/v1/facebook", (req, res)=>{
+    const instaSocial = {
+        username : 'pavamandabeer',
+        followers : '70',
+        follows : '150',
+        date : Date.now()
+    };
+
+    res.status(200).json(instaSocial)
+})
+
+app.get("/api/v1/linkedin", (req, res)=>{
+    const instaSocial = {
+        username : 'pavamandabeer',
+        followers : '150',
+        follows : '30',
+        date : Date.now()
+    };
+
+    res.status(200).json(instaSocial)
 })
 
 app.listen(PORT, ()=>{
