7f4a5f8626998f66217f4c58422444e0e1a25f07
Pavaman Dabeer <pavamandabeer@gmail.com>
2022-08-31 21:55:59 +0530

    install swagger package, and use it in the project and wrote documentation yaml file as well

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const format = require('date-format');
 
 const app = express();
 
+
+//Swagger Docs related
+
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+
 const PORT = process.env.PORT || 4000
 
 
