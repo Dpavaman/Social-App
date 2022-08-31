904502e5ea519236138dc1317b15212c14fce5ed
Pavaman Dabeer <67939138+Dpavaman@users.noreply.github.com>
2022-08-31 04:37:21 -0700

    All social routes (#1)
    * added routes for different social medias
    
    * format Date using date-format package
    
    * added params route
    
    * .gitignore refactored

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const format = require('date-format');
 
 const app = express();
 
@@ -6,7 +7,45 @@ const PORT = process.env.PORT || 4000
 
 
 app.get('/', (req, res)=>{
-    res.send("Hello World!")
+    res.status(200).send("Hello World!")
+})
+
+app.get("/api/v1/instagram", (req, res)=>{
+    const instaSocial = {
+        username : 'pavamandabeer',
+        followers : '100',
+        follows : '400',
+        date : format.asString("dd/MM/yyyy - hh:mm:ss", new Date())
+    };
+
+    res.status(200).json(instaSocial)
+})
+
+app.get("/api/v1/facebook", (req, res)=>{
+    const instaSocial = {
+        username : 'pavamandabeer',
+        followers : '70',
+        follows : '150',
+        date : format.asString("dd/MM/yyyy - hh:mm:ss", new Date())
+    };
+
+    res.status(200).json(instaSocial)
+})
+
+app.get("/api/v1/linkedin", (req, res)=>{
+    const instaSocial = {
+        username : 'pavamandabeer',
+        followers : '150',
+        follows : '30',
+        date : format.asString("dd/MM/yyyy - hh:mm:ss", new Date())
+    };
+
+    res.status(200).json(instaSocial)
+})
+
+
+app.get('/api/v1/:token', (req, res)=>{
+    res.status(200).json({param : req.params.token})
 })
 
 app.listen(PORT, ()=>{
